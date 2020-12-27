b5b90469488bd1048ebbe6632683df89aaf7be38
fede tanco <63819426+fedetanco22@users.noreply.github.com>
2020-12-27 12:50:51 -0300

    Update app.js

--- js/app.js
+++ js/app.js
@@ -3,7 +3,7 @@ document.addEventListener('DOMContentLoaded', cargar);
 
 function cargar() {
     $.ajax({
-        url: '/js/data.json',
+        url: './js/data.json',
         dataType:'json'
         success: function (dataBase) { // console.log(data)
             data = dataBase;
